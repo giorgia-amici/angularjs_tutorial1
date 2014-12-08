befe16080766e8cbd4122cc2e397a1489f1c3a99
Giorgia <giorgia1london@gmail.com>
2014-12-08 22:59:47 +0000

    at

--- js/MainController.js
+++ js/MainController.js
@@ -1,6 +1,7 @@
 (function() {
       
       var app = angular.module("gitHubViewer", []);
+
       var MainController = function($scope, $http) {
 
         var onUserComplete = function(response) {
@@ -11,7 +12,7 @@
           $scope.error = "Could not fetch the user";
         };
 
-        $http.get("https://api.github.com/users/giorgia-amici/access_token=ff8a6e78e60868859c7a61a800aaf3cb46885725").then(onUserComplete, onError);
+        $http.get("https://api.github.com/users/giorgia-amici).then(onUserComplete, onError);
 
         $scope.message = 1 + 1;
 
